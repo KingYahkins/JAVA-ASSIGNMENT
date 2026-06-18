89ea6afba6bd257edbccf1ead4a4104af171a79b
KingYahkins <yliezeryahkins@gmail.com>
2026-06-18 15:15:38 +0000

    casing

---
+++ src/Casing.java
@@ -0,0 +1,18 @@
+public class Casing {
+    public static void main(String[] args) {
+
+        // auto casing
+        int y  = 10;
+
+        double a=y;
+
+        //display
+        System.out.println("value: "+ a);
+
+        //manual
+        double r= 10.00;
+        int n = (int) r;
+        // display
+        System.out.println(n);
+    }
+}
