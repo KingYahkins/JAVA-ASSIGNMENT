107b32ec412fbca06f19b18ec7bb9de7033bccf0
KingYahkins <yliezeryahkins@gmail.com>
2026-06-18 15:06:42 +0000

    data types

---
+++ src/DATATYPES.java
@@ -0,0 +1,15 @@
+public class DATATYPES {
+    public static void main(String[] args) {
+        //String
+        String name ="Wesnoff";
+        int age = 10;
+        double amount = 10;
+        //using println
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+age);
+        System.out.println("amount: "+amount);
+
+        //using printf
+        System.out.printf("My name is %s.I am %d years old. I take home GHC %.2f",name,age,amount);
+    }
+}
